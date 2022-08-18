5eee268f302ee95791f1fcea6df013eec26e7690
lucassmann <lucas.assmann@edu.pucrs.br>
2022-08-18 20:33:41 -0300

    addPerson on local storage

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ const createPerson = (person) => {
     setLocalStorage(personStorage)
 }
 
+// Event Handlers
+
+const addPersonEvent = () => {
+    if (isValidFields()) {
+        const person = {
+            name: document.getElementById('name').value,
+            age: document.getElementById('age').value,
+        }
+        const index = document.getElementById('name').dataset.index
+            createPerson(person)}
+}
+
+const isValidFields = () => {
+    return document.getElementById('addPersonForm').reportValidity()
+}
+
+// Event Listeners
+
+document.getElementById('addPerson')
+    .addEventListener('click', addPersonEvent)
